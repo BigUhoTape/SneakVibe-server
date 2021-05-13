7fc09ba8df288860e6dc9db51ada22ec19f48bd9
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2021-05-13 22:49:12 +0700

    order index

--- modules/api/controllers/OrderController.php
+++ modules/api/controllers/OrderController.php
@@ -3,6 +3,8 @@
 namespace app\modules\api\controllers;
 
 use app\modules\api\models\OrderForm;
+use app\modules\api\resources\OrderResource;
+use yii\data\ActiveDataProvider;
 
 class OrderController extends ActiveController {
 
@@ -11,9 +13,27 @@ class OrderController extends ActiveController {
     public function actions()
     {
         $actions = parent::actions();
+        unset($actions['index']);
         unset($actions['delete']);
         unset($actions['update']);
         return $actions;
+    }
+
+    public function actionIndex () {
+        $query = OrderResource::find()
+            ->andWhere(['user_id' => \Yii::$app->getUser()->getId()]);
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'sort' => [
+                'defaultOrder' => [ 'created_at' => SORT_DESC ]
+            ],
+            'pagination' => [
+                'pageSize' => 2
+            ]
+        ]);
+
+        return $dataProvider;
     }
 
 }
